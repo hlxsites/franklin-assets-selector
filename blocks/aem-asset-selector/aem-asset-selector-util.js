0b7566c201a09aca25f8fecb072697995408690d
tmathern <60901087+tmathern@users.noreply.github.com>
2023-08-25 20:06:57 -0700

    With directories (#59)
    * With directories

--- blocks/aem-asset-selector/aem-asset-selector-util.js
+++ blocks/aem-asset-selector/aem-asset-selector-util.js
@@ -390,6 +390,10 @@ export async function renderAssetSelectorWithImsFlow(cfg) {
             readOnly: true,
             options: [
               {
+                label: 'Directories',
+                value: 'directory',
+              },
+              {
                 label: 'Images',
                 value: 'image/*',
               },
