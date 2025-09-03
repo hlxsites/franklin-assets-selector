ef73e8377d15164046d694350f760aef59a53414
gazal2708 <gazaljain7523@gmail.com>
2025-09-03 12:05:45 +0530

    testing assets plugin change

--- plugins/aem-assets-plugin/scripts/aem-assets.js
+++ plugins/aem-assets-plugin/scripts/aem-assets.js
@@ -81,7 +81,7 @@ function createWebOptimizedDMOpenAPIUrl(url) {
  */
 function getImageSrcUrlAndAlt(element) {
   if (element.tagName === 'A') {
-    return { url: element.getAttribute('href'), alt: '' };
+    return { url: element.getAttribute('href'), alt: element.getAttribute('title') || '' };
   }
 
   if (element.tagName === 'IMG') {
